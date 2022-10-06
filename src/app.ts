4bcb242c2a5b61a978c389f8b7234a3a223e252d
Leonardoozx <leonardo2025maldarck@gmail.com>
2022-10-06 19:30:17 -0300

    feat: new route for listing all the orders

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import productsRouter from './routes/product.routes';
 import usersRouter from './routes/user.routes';
+import orderRouter from './routes/order.routes';
 
 const app = express();
 
@@ -8,5 +9,6 @@ app.use(express.json());
 
 app.use('/products', productsRouter);
 app.use('/users', usersRouter);
+app.use('/orders', orderRouter);
 
 export default app;
