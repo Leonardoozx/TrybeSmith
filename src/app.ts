c5c18bad8eef69922d2ce8ab98ff136650384b8b
Leonardoozx <leonardo2025maldarck@gmail.com>
2022-10-06 20:29:51 -0300

    feat: new route for making login

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import productsRouter from './routes/product.routes';
 import usersRouter from './routes/user.routes';
 import orderRouter from './routes/order.routes';
+import loginRouter from './routes/login.routes';
 
 const app = express();
 
@@ -10,5 +11,6 @@ app.use(express.json());
 app.use('/products', productsRouter);
 app.use('/users', usersRouter);
 app.use('/orders', orderRouter);
+app.use('/login', loginRouter);
 
 export default app;
