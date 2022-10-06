952b9dcac3017477b37b56065225dd1e37f9c6f0
Leonardoozx <leonardo2025maldarck@gmail.com>
2022-10-06 17:41:34 -0300

    refactor: changes product service name

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -1,12 +1,16 @@
 import { Request, Response } from 'express';
 import statusCodes from '../statusCodes';
-import ProductsService from '../services/product.service';
+import ProductService from '../services/product.service';
 
 export default class ProductsController {
-  constructor(private productService = new ProductsService()) {}
+  constructor(private productService = new ProductService()) {}
 
-  insertProduct = async (req: Request, res: Response) => {
+  public insertProduct = async (req: Request, res: Response) => {
     const newProduct = await this.productService.insertProduct(req.body);
     res.status(statusCodes.CREATED).json(newProduct);
+  };
+
+  public getAllProducts = async (_req: Request, res: Response) => {
+    res.status(statusCodes.OK).json(await this.productService.getAllProducts());
   };
 }
