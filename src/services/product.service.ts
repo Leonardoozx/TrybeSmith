78eb54b5d9c91e9b87098c2a3aca1f5aaded472e
Leonardoozx <leonardo2025maldarck@gmail.com>
2022-10-06 17:19:49 -0300

    faet: a service for creating new products in db

---
+++ src/services/product.service.ts
@@ -0,0 +1,13 @@
+import connection from '../models/connection';
+import ProductModel from '../models/product.model';
+import Product from '../interfaces/product.interface';
+
+export default class ProductService {
+  public model: ProductModel;
+
+  constructor() {
+    this.model = new ProductModel(connection);
+  }
+
+  public insertProduct = (product: Product): Promise<Product> => this.model.insertProduct(product);
+}
