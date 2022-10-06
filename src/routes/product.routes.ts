aeeab97a48919523a73ac1f5aca25e6b983b6693
Leonardoozx <leonardo2025maldarck@gmail.com>
2022-10-06 20:45:57 -0300

    feat: put all the product middlewares to work, lol

--- src/routes/product.routes.ts
+++ src/routes/product.routes.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express';
 import ProductsController from '../controllers/product.controller';
+import { validateAmountField, validateNameField } from '../middlewares/product.middlewares';
 
 const productsController = new ProductsController();
 
 const router = Router();
 
 router.get('/', productsController.getAllProducts);
-router.post('/', productsController.insertProduct);
+router.post('/', validateNameField, validateAmountField, productsController.insertProduct);
 
 export default router;
