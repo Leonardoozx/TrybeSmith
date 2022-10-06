e152b99d2d973f3545a8554f7cf0b97b3ee2be15
Leonardoozx <leonardo2025maldarck@gmail.com>
2022-10-06 18:24:30 -0300

    feat: new route for inserting users

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import productsRouter from './routes/product.routes';
+import usersRouter from './routes/user.routes';
 
 const app = express();
 
 app.use(express.json());
 
 app.use('/products', productsRouter);
+app.use('/users', usersRouter);
 
 export default app;
