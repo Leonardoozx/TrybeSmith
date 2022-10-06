9350f4b9d15b2b9f401a7447379225b011dcb2e6
Leonardoozx <leonardo2025maldarck@gmail.com>
2022-10-06 20:58:09 -0300

    feat: all the middlewares are in their right places

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,10 +1,23 @@
 import { Router } from 'express';
 import UserController from '../controllers/user.controller';
+import {
+  validateClasseField,
+  validateLevelField,
+  validatePasswordField,
+  validateUsernameField,
+} from '../middlewares/user.middlewares';
 
 const userController = new UserController();
 
 const router = Router();
 
-router.post('/', userController.insertUser);
+router.post(
+  '/',
+  validateUsernameField,
+  validateClasseField,
+  validateLevelField,
+  validatePasswordField,
+  userController.insertUser,
+);
 
 export default router;
