f389b9d5886d96db2f11f3625db8e81f65533dd2
Leonardoozx <leonardo2025maldarck@gmail.com>
2022-10-07 14:40:59 -0300

    a route for inserting orders

--- src/routes/order.routes.ts
+++ src/routes/order.routes.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import OrderController from '../controllers/order.controller';
+import validateToken from '../middlewares/validateToken.middleware';
 
 const orderController = new OrderController();
 
 const router = Router();
 
 router.get('/', orderController.getAllOrders);
+router.post('/', validateToken, orderController.insertOrder);
 
 export default router;
