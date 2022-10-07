d4be75ba85186f82d6fcc6f023933685947450a6
Leonardoozx <leonardo2025maldarck@gmail.com>
2022-10-07 16:28:46 -0300

    feat: now the token validator middleware adds a property key at req

--- src/middlewares/validateToken.middleware.ts
+++ src/middlewares/validateToken.middleware.ts
@@ -1,13 +1,17 @@
 import * as jwt from 'jsonwebtoken';
-import { NextFunction, Request, Response } from 'express';
+import { NextFunction, Response } from 'express';
 import statusCodes from '../statusCodes';
 import { JWTsecret } from '../auth/createJWT';
+import RequestWithUsername from '../interfaces/requestWithUsername.interface';
 
-const validateToken = (req:Request, res:Response, next: NextFunction) => {
+const validateToken = (req: RequestWithUsername, res:Response, next: NextFunction) => {
   const token = req.header('authorization');
   if (!token) return res.status(statusCodes.UNAUTHORIZED).json({ message: 'Token not found' });
-  const tokenValidation = jwt.verify(token, JWTsecret);
-  if (!tokenValidation) {
+  try {
+    const tokenValidation = jwt.verify(token, JWTsecret);
+    const { data } = tokenValidation as { data: string };
+    req.username = data[0] as string;
+  } catch (_e) {
     return res.status(statusCodes.UNAUTHORIZED).json({ message: 'Invalid token' });
   }
   next();
