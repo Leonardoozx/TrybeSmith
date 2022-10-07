9c1ef06297054a407e9900158a2c298c51a45a50
Leonardoozx <leonardo2025maldarck@gmail.com>
2022-10-07 14:40:31 -0300

    feat: controller for inserting new orders

--- src/controllers/order.controller.ts
+++ src/controllers/order.controller.ts
@@ -5,7 +5,11 @@ import OrderService from '../services/order.service';
 export default class OrderController {
   constructor(private orderService = new OrderService()) { }
 
-  getAllOrders = async (_req: Request, res: Response) => {
+  public getAllOrders = async (_req: Request, res: Response) => {
     res.status(statusCodes.OK).json(await this.orderService.getAllOrders());
+  };
+
+  public insertOrder = async (req:Request, res:Response) => {
+    res.status(statusCodes.CREATED).json({ message: 'is working' });
   };
 }
